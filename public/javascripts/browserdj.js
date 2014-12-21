0c5e0a144451d81f587414a09271afc54978e60e
sl2 <dimension.sf@gmail.com>
2014-12-21 13:47:24 +0900

    many bugs are fixed

--- public/javascripts/browserdj.js
+++ public/javascripts/browserdj.js
@@ -9,13 +9,60 @@ $(function(){
     })();
 
     var context = new window.AudioContext();
+    
+    var deck1 = createDeck(); 
+    var deck2 = createDeck(); 
+    function createDeck() {
+        return {
+            source : context.createBufferSource(),
+            analyser : context.createAnalyser(),
+            panner : context.createPanner(),
+            lowpass : context.createBiquadFilter(),
+            highpass : context.createBiquadFilter(),
+            gain : context.createGain(),
+            scriptProcessor : context.createScriptProcessor(2048, 1, 1),
+            playing : false
+        };
+    }
+
+    function connectDeck(deck, buffer, startTime) {
+        deck.source = context.createBufferSource();
+        deck.source.buffer = buffer;
+        deck.source.loop = true;
+        
+        deck.gain.gain.value = 1;
+         
+        deck.lowpass.type = "lowpass";
+        deck.lowpass.frequency.value = 30000;
+        deck.lowpass.Q.value = 5;
+         
+        deck.highpass.type = "highpass";
+        deck.highpass.frequency.value = 0;
+        deck.highpass.Q.value = 5;
+        
+        deck.panner.setPosition(0,0,0);
+
+        deck.analyser.smoothingTimeConstant = 0.9;
+        deck.analyser.fftSize = 128;
+
+        deck.source.connect(deck.gain);
+        deck.gain.connect(deck.lowpass);
+        deck.lowpass.connect(deck.highpass);
+        deck.highpass.connect(deck.panner); 
+        deck.panner.connect(deck.analyser);
+        deck.panner.connect(context.destination);
+        deck.analyser.connect(deck.scriptProcessor);
+        deck.scriptProcessor.connect(context.destination);
+        
+        deck.source.start(startTime);
+        return deck;
+    }
+
     var buffer1;
     var buffer2;
 
     function loadAudioFile(track){
         var item = document.getElementById('input_file_track' + track).files[0];
-        var info = 'name:' + item.name + ' type:' + item.type + ' size:' + item.size;
-        
         var trackNum = track;
         var trackName = item.name;
         
@@ -54,7 +101,6 @@ $(function(){
                         break;
                 }
             }, function(err) {
-                console.log(err);
                 alert('Decode Error');
             });
         };
@@ -65,146 +111,69 @@ $(function(){
         }, 1500);
     }
 
-    //Nodes 
-    var source1 = context.createBufferSource();
-    var analyser1 = context.createAnalyser();
-    var panner1 = context.createPanner();
-    var lowpass1 = context.createBiquadFilter();
-    var highpass1 = context.createBiquadFilter();
-    var jsNode1 = context.createScriptProcessor(2048, 1, 1);
-    var gain1 = context.createGain();
-
-    var source2 = context.createBufferSource();
-    var analyser2 = context.createAnalyser();
-    var panner2 = context.createPanner();
-    var lowpass2 = context.createBiquadFilter();
-    var highpass2 = context.createBiquadFilter();
-    var jsNode2 = context.createScriptProcessor(2048, 1, 1);
-    var gain2 = context.createGain();
-   
-    function playSound(buffer, time) {
-        var source = context.createBufferSource();
-        var analyser = context.createAnalyser();
-        var panner = context.createPanner();
-        var lowpass = context.createBiquadFilter();
-        var highpass = context.createBiquadFilter();
-        var gain = context.createGain();
-        var jsNode = context.createScriptProcessor(2048, 1, 1);
-        jsNode.connect(context.destination);
-        
-        source.connect(gain);
-        gain.gain.value = 1;
-
-        //Nodes configuration
-        source.buffer = buffer;
-        source.loop = true;
- 
-        panner.setPosition(0,0,0);
-         
-        lowpass.type = "lowpass";
-        lowpass.frequency.value = 30000;
-        lowpass.Q.value = 20;
-         
-        highpass.type = "highpass";
-        highpass.frequency.value = 0;
-        highpass.Q.value = 20;
-
-        analyser.smoothingTimeConstant = 1; //1:Max smoothness
-        analyser.fftSize = 128;
-        source.connect(analyser);
-
-        //Node connection
-        source.connect(lowpass);
-        lowpass.connect(highpass);
-        highpass.connect(panner);
-         
-        panner.connect(gain);
-        gain.connect(context.destination);
-       
-        source.start(time); 
-        
-        return {
-            source:source,
-            gain:gain,
-            analyser:analyser,
-            panner:panner,
-            lowpass:lowpass,
-            highpass:highpass,
-            jsNode:jsNode
-        };
-    }
-
     function play1() {
-        var startTime = context.currentTime + 0.100;
-        var rt = playSound(buffer1, startTime);
+        if (!deck1.playing) {
+            deck1.playing = true;
 
-        source1 = rt.source;
-        gain1 = rt.gain;
-        panner1 = rt.panner;
-        compressor1 = rt.compressor;
-        lowpass1 = rt.lowpass;
-        highpass1 = rt.highpass;
-        analyser1 = rt.analyser;
-        jsNode1 = rt.jsNode;
-        jsNode1.onaudioprocess = function(){
-            var freq; 
-            var formattedFreq = [];
-            
-            if (analyser1){ 
-                freq = new Uint8Array(analyser1.frequencyBinCount);
-                analyser1.getByteTimeDomainData(freq);
-                for(var i = 0; i<freq.length; i++){
-                    formattedFreq.push({val:freq[i]}); 
+            var startTime = context.currentTime + 0.100;
+            deck1 = connectDeck(deck1, buffer1, startTime);
+            deck1.scriptProcessor.onaudioprocess = function(){
+                var freq; 
+                var formattedFreq = [];
+                if (deck1.analyser){ 
+                    freq = new Uint8Array(deck1.analyser.frequencyBinCount);
+                    deck1.analyser.getByteTimeDomainData(freq);
+                    for(var i = 0; i<freq.length; i++){
+                        formattedFreq.push({val:freq[i]}); 
+                    }
                 }
-            }
-            drawEQ('#viz1', formattedFreq);
-        };
-        calcVolume();
-        calcEffect();
+                drawEQ('#viz1', formattedFreq);
+            };
+            calcVolume();
+            calcEffect();
+        }
     }
 
     function play2() {
-        var startTime = context.currentTime + 0.100;
-        var rt = playSound(buffer2, startTime);
+        if (!deck2.playing) {
+            deck1.playing = true;
 
-        source2 = rt.source;
-        gain2 = rt.gain;
-        panner2 = rt.panner;
-        compressor2 = rt.compressor;
-        lowpass2 = rt.lowpass;
-        highpass2 = rt.highpass;
-        analyser2 = rt.analyser;
-        jsNode2 = rt.jsNode;
-        jsNode2.onaudioprocess = function(){
-            var freq; 
-            var formattedFreq = [];
-            
-            if (analyser2){ 
-                freq = new Uint8Array(analyser2.frequencyBinCount);
-                analyser2.getByteTimeDomainData(freq);
-                for(var i = 0; i<freq.length; i++){
-                    formattedFreq.push({val:freq[i]}); 
+            var startTime = context.currentTime + 0.100;
+            deck2 = connectDeck(deck2, buffer2, startTime);
+            deck2.scriptProcessor.onaudioprocess = function(){
+                var freq; 
+                var formattedFreq = [];
+                if (deck2.analyser){ 
+                    freq = new Uint8Array(deck2.analyser.frequencyBinCount);
+                    deck2.analyser.getByteTimeDomainData(freq);
+                    for(var i = 0; i<freq.length; i++){
+                        formattedFreq.push({val:freq[i]}); 
+                    }
                 }
-            }
-            drawEQ('#viz2', formattedFreq);
-        };
-        calcVolume();
-        calcEffect();
+                drawEQ('#viz2', formattedFreq);
+            };
+            calcVolume();
+            calcEffect();
+        }
     }
-    
+ 
     function stop1() {
-        source1.stop(0);
-    }   
-    
+        if (deck1.playing) {
+            deck1.source.stop(0);
+            deck1.playing = false;
+        }
+    } 
+
     function stop2() {
-        source2.stop(0);
-    }
-    
+        if (deck2.playing) {
+            deck2.source.stop(0);
+            deck2.playing = false;
+        }
+    } 
+
     function drawEQ(elem, data){
         d3.select(elem).select('svg').remove();
         
-        //////////////////////////////////////////////
-
         var bS = 1; //Bar space
         var bW = 5; //Bar width
         var bH = 80; //Maximum height of bars
@@ -240,18 +209,17 @@ $(function(){
         var vol1 = parseFloat($('#playvolume_display1').html());
         var vol2 = parseFloat($('#playvolume_display2').html());
         var vol = 0;
-        console.log(gain1.gain.value); 
         if(fader > 0){
             vol = vol1 * (1 - fader);
-            gain1.gain.value = vol;
-            gain2.gain.value = vol2;
+            deck1.gain.gain.value = vol;
+            deck2.gain.gain.value = vol2;
         }else if(fader < 0){
             vol = vol2 * (1 + fader);
-            gain2.gain.value = vol;
-            gain1.gain.value = vol1;
+            deck2.gain.gain.value = vol;
+            deck1.gain.gain.value = vol1;
         }else if(fader === 0){
-            gain1.gain.value = vol1;
-            gain2.gain.value = vol2;
+            deck1.gain.gain.value = vol1;
+            deck2.gain.gain.value = vol2;
         }
     }
     
@@ -270,19 +238,19 @@ $(function(){
         var _panner2X = parseFloat($('#panner2X_display').html());
         var _panner2Y = parseFloat($('#panner2Y_display').html());
         
-        panner1.setPosition(_panner1X,0,-1);
-        panner1.setPosition(0, _panner1Y,-1);
-        panner2.setPosition(_panner2X,0,-1);
-        panner2.setPosition(0, _panner2Y,-1);
+        deck1.panner.setPosition(_panner1X,0,-1);
+        deck1.panner.setPosition(0, _panner1Y,-1);
+        deck2.panner.setPosition(_panner2X,0,-1);
+        deck2.panner.setPosition(0, _panner2Y,-1);
         
-        lowpass1.frequency.value = _lowpass1;
-        lowpass2.frequency.value = _lowpass2;
+        deck1.lowpass.frequency.value = _lowpass1;
+        deck2.lowpass.frequency.value = _lowpass2;
         
-        highpass1.frequency.value = _highpass1;
-        highpass2.frequency.value = _highpass2;
+        deck1.highpass.frequency.value = _highpass1;
+        deck2.highpass.frequency.value = _highpass2;
         
-        source1.playbackRate.value = _speed1;
-        source2.playbackRate.value = _speed2;
+        deck1.source.playbackRate.value = _speed1;
+        deck2.source.playbackRate.value = _speed2;
     }
 
     $('#button_open_track1').click(function(){
@@ -301,41 +269,20 @@ $(function(){
         loadAudioFile(2); 
     });
 
-    var State;
-    (function(){
-      var instance;
-      State = function State(){
-          if (instance) {
-              return instance;
-          }
-          instance = this;
-          this.track1Play = false;
-          this.track2Play = false;
-      };
-    }());
-
     $('#play1').click(function(){
-        if (!State.track1Play){
-            play1();
-            State.track1Play = true;
-        }
+        play1();
     });
     
     $('#stop1').click(function(){
         stop1();
-        State.track1Play = false;
     });
 
     $('#play2').click(function(){
-        if (!State.track2Play){
-            play2();
-            State.track2Play = true;
-        }
+        play2();
     });
 
     $('#stop2').click(function(){
         stop2();
-        State.track2Play = false;
     });
 
     //Slider
@@ -351,7 +298,6 @@ $(function(){
         }
     }).css('width', 300).css('height', 0).css('top','8px');
 
-
     $('#playspeed_slider1').slider({
         orientation: 'vertical',
         min: 0.1,
@@ -361,7 +307,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#playspeed_display1').html( ui.value );
-            source1.playbackRate.value = ui.value;
+            deck1.source.playbackRate.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
 
@@ -374,7 +320,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#playspeed_display2').html( ui.value );
-            source2.playbackRate.value = ui.value;
+            deck2.source.playbackRate.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
     
@@ -412,7 +358,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#panner1X_display').html( ui.value );
-            panner1.setPosition(ui.value,0,-1);
+            deck1.panner.setPosition(ui.value,0,-1);
         }
     }).css('width', 80).css('height', 0);
 
@@ -424,7 +370,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#panner2X_display').html( ui.value );
-            panner2.setPosition(ui.value,0,-1);
+            deck2.panner.setPosition(ui.value,0,-1);
         }
     }).css('width', 80).css('height', 0);
 
@@ -437,7 +383,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#panner1Y_display').html( ui.value );
-            panner1.setPosition(0, ui.value,-1);
+            deck1.panner.setPosition(0, ui.value,-1);
         }
     }).css('height', 80).css('width', 0);
 
@@ -450,7 +396,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#panner2Y_display').html( ui.value );
-            panner2.setPosition(0, ui.value,-1);
+            deck2.panner.setPosition(0, ui.value,-1);
         }
     }).css('height', 80).css('width', 0);
 
@@ -464,7 +410,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#lowpass_display1').html( ui.value );
-            lowpass1.frequency.value = ui.value;
+            deck1.lowpass.frequency.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
 
@@ -477,7 +423,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#lowpass_display2').html( ui.value );
-            lowpass2.frequency.value = ui.value;
+            deck2.lowpass.frequency.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
 
@@ -490,9 +436,8 @@ $(function(){
         step: 10,
         animate: 'slow',
         slide: function(event, ui){
-            
             $('#highpass_display1').html( ui.value );
-            highpass1.frequency.value = ui.value;
+            deck1.highpass.frequency.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
 
@@ -505,7 +450,7 @@ $(function(){
         animate: 'slow',
         slide: function(event, ui){
             $('#highpass_display2').html( ui.value );
-            highpass2.frequency.value = ui.value;
+            deck2.highpass.frequency.value = ui.value;
         }
     }).css('height', 170).css('width', 0);
    
@@ -518,6 +463,4 @@ $(function(){
     lcd2.write2Display('lettersRL01','TrackB');
 
 });
-
-
 
